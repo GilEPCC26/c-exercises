99fb1e069a40b13dd65dd5204268344c0961cc8f
GilEPCC26 <ggonz441@my.epcc.edu>
2025-11-26 16:01:37 -0700

    Implement input validation and array population
    Added input validation for array length and implemented reading values into the array.

--- 11-arrays-reverse-print.c
+++ 11-arrays-reverse-print.c
@@ -25,10 +25,36 @@ int main(void) {
     int length = 0;
 
     // TODO: ask for length and ensure it is between 1 and MAX_LENGTH.
+    do {
+        printf("How many numbers (1-%d)? ", MAX_LENGTH);
+        scanf("%d", &length);
+        if (length < 1 || length > MAX_LENGTH) {
+            printf("Invalid length. Please enter a number between 1 and %d.\n", MAX_LENGTH);
+        }
+    } while (length < 1 || length > MAX_LENGTH);
 
     // TODO: read values into the array using a loop.
+    for (int i = 0; i < length; i++) {
+        printf("Value %d: ", i + 1);
+        scanf("%d", &data[i]);
+    }
 
     // TODO: print values in original order, then print them in reverse order.
+
+    // Print in original order (forward loop)
+    printf("You entered: ");
+    for (int i = 0; i < length; i++) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+
+    // Print in reverse order (backward loop)
+    printf("Reversed: ");
+    // Start index at the last element (length - 1) and decrement to 0
+    for (int i = length - 1; i >= 0; i--) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
 
     return 0;
 }
